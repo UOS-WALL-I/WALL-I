e0cc31a49904be6b1f04f54c6f9a76cb73c3092e
Min-h-96 <asdf6256@gmail.com>
2021-12-19 14:17:47 +0900

    initial commit

---
+++ src/main/java/NPC/walli/WallIApplication.java
@@ -0,0 +1,13 @@
+package NPC.walli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NPC/walli/WallIApplicationTests.java
@@ -0,0 +1,13 @@
+package NPC.walli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
